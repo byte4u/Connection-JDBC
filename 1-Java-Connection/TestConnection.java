2937bb3eada97c543c4d76ec1bd7b88f41897ecb
byte4u <77403094+byte4u@users.noreply.github.com>
2021-04-05 21:05:47 -0300

    Java Connection

---
+++ 1-Java-Connection/TestConnection.java
@@ -0,0 +1,15 @@
+package connection;
+
+/**
+ * Connection class
+ *
+ * @author Samadhi
+ * @version 1
+ * @date 2021/04/05
+ */
+public class TestConnection {
+
+    public static void main(String[] args) {
+        MyConnection.toConnect();
+    }
+}
